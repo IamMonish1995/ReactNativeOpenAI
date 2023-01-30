3580cad7249873ba1f5104dd172b9829ebb9455d
monish <monishbarse9@gmail.com>
2023-01-31 00:59:44 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TouchableOpacity} from 'react-native';
+import { Button,Colors } from 'react-native-ui-lib';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={() => console.log('pressed')}>
+      <Button label={'Press'} size={Button.sizes.medium} backgroundColor={Colors.red30}/>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
